da1529b65fae8676d68f4b7c5446a2092def04a4
Marcos Pinho <marcosjava2008@gmail.com>
2018-10-20 20:21:26 -0300

    Implementando testes de aceitação

--- src/test/java/com/marcospinho/demo/bdd/steps/PessoaStep.java
+++ src/test/java/com/marcospinho/demo/bdd/steps/PessoaStep.java
@@ -1,18 +1,25 @@
 package com.marcospinho.demo.bdd.steps;
 
+import com.marcospinho.demo.DemoTestesAutomatizadosApplication;
 import com.marcospinho.demo.entity.Pessoa;
 import com.marcospinho.demo.service.PessoaService;
-import com.marcospinho.demo.service.impl.PessoaServiceImpl;
 import cucumber.api.java.Before;
 import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.E;
 import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.test.context.ContextConfiguration;
 
 /**
  * @author Marcos Pinho
  */
+@ContextConfiguration(classes = DemoTestesAutomatizadosApplication.class, loader = SpringBootContextLoader.class)
 public class PessoaStep {
 
+    @Autowired
     private PessoaService service;
 
     private Pessoa pessoa;
@@ -20,7 +27,6 @@ public class PessoaStep {
     @Before
     public void init() {
         this.pessoa = new Pessoa();
-        this.service = new PessoaServiceImpl();
     }
 
     @Dado("^uma pessoa com o nome (.*)$")
@@ -28,17 +34,23 @@ public class PessoaStep {
         this.pessoa.setNome(nome);
     }
 
+    @E("^com a idade (\\d*)$")
+    public void comAIdade(Integer idade) throws Throwable {
+        this.pessoa.setIdade(idade);
+    }
+
     @Quando("^tento cadastrá-la$")
     public void tentoCadastraLa() throws Throwable {
         this.service.create(this.pessoa);
     }
 
-    @Então("^Falha: Nome da pessoa é obrigatória$")
-    public void falhaNomeDaPessoaEhObrigatoria() throws Throwable {
+    @Então("^Informações obrigatórias ainda não foram informadas$")
+    public void informaçõesObrigatóriasAindaNãoForamInformadas() throws Throwable {
     }
 
-    @Então("^Pessoa cadastrada com sucesso$")
-    public void pessoaCadastradaComSucesso() throws Throwable {
+    @Então("^Pessoa cadastrada com ([a-z A-Z]*)$")
+    public void pessoaCadastradaComSucesso(String resultado) throws Throwable {
+        Assert.assertEquals(resultado, "sucesso");
     }
 
 }
--- src/test/resources/bdd/pessoa.feature
+++ src/test/resources/bdd/pessoa.feature
@@ -4,17 +4,19 @@ Funcionalidade: Cadastrar uma nova pessoa
     Eu logado no sistema
     Desejo cadastrar uma nova pessoa
     A fim de obter suas informações para futuras tomadas de decisão
-     
-    Esquema do Cenário: Garantir que uma nova pessoa só possa ser cadastrada caso seu nome seja informado
+
+    Esquema do Cenário: Garantir que uma nova pessoa possa ser cadastrada no sistema
         Dado uma pessoa com o nome <nome>
+        E com a idade <idade>
         Quando tento cadastrá-la
         Então <resultado>
-            
+
         Exemplos: 
-            |     nome          |                resultado                  |
-            |     ""            |    Falha: Nome da pessoa é obrigatória    |
-            |     null          |    Falha: Nome da pessoa é obrigatória    |
-            |     Marcos        |    Pessoa cadastrada com sucesso          |
+            |     nome          |   idade     |                         resultado                           |
+#            |                   |   null      |       Informações obrigatórias ainda não foram informadas   |
+#            |     Marcos        |   null      |       Informações obrigatórias ainda não foram informadas   |
+#            |                   |    32       |       Informações obrigatórias ainda não foram informadas   |
+            |     Marcos        |    32       |       Pessoa cadastrada com sucesso                         |
 
     
     
