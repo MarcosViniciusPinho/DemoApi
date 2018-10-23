8e77422a676959687c711e347ac43157477e0658
Marcos Pinho <marcosjava2008@gmail.com>
2018-10-22 22:25:37 -0300

    Implementando ajustes nos testes de unidade.

--- src/test/java/com/marcospinho/demo/unit/PessoaServiceImplTest.java
+++ src/test/java/com/marcospinho/demo/unit/PessoaServiceImplTest.java
@@ -2,6 +2,7 @@ package com.marcospinho.demo.unit;
 
 import com.marcospinho.demo.entity.Pessoa;
 import com.marcospinho.demo.repository.PessoaRepository;
+import com.marcospinho.demo.service.exception.RecurseNotFoundException;
 import com.marcospinho.demo.service.impl.PessoaServiceImpl;
 import org.junit.Assert;
 import org.junit.Test;
@@ -27,7 +28,7 @@ public class PessoaServiceImplTest {
     private PessoaRepository repository;
 
     @Test
-    public void testCreate() {
+    public void testCreateSucess() {
         Pessoa pessoaExecucao = new Pessoa(null, "Marcos", "Pinho", 32);
         Pessoa pessoaEsperada = new Pessoa(1L, "Marcos", "Pinho", 32);
         Mockito.when(this.repository.save(pessoaExecucao)).thenReturn(pessoaEsperada);
@@ -35,8 +36,16 @@ public class PessoaServiceImplTest {
         Assert.assertEquals(pessoaEsperada, pessoaSalva.get());
     }
 
+    @Test(expected = NullPointerException.class)
+    public void testCreateComException() {
+        Pessoa pessoaExecucao = new Pessoa(null, "Marcos", "Pinho", 32);
+        Pessoa pessoaEsperada = new Pessoa(1L, "Marcos", "Pinho", 32);
+        Optional pessoaSalva = this.service.create(pessoaExecucao);
+        Assert.assertEquals(pessoaEsperada, pessoaSalva.get());
+    }
+
     @Test
-    public void testUpdate() {
+    public void testUpdateSucess() {
         Pessoa pessoaExecucao = new Pessoa(1L, "Marcos2", "Pinho2", 33);
         Optional<Pessoa> pessoaEsperada = Optional.of(new Pessoa(1L, "Marcos2", "Pinho2", 33));
         Mockito.when(this.repository.findById(pessoaExecucao.getId())).thenReturn(pessoaEsperada);
@@ -45,8 +54,17 @@ public class PessoaServiceImplTest {
         Assert.assertEquals(pessoaEsperada, pessoaSalva);
     }
 
+    @Test(expected = RecurseNotFoundException.class)
+    public void testUpdateComException() {
+        Pessoa pessoaExecucao = new Pessoa(1L, "Marcos2", "Pinho2", 33);
+        Optional<Pessoa> pessoaEsperada = Optional.of(new Pessoa(1L, "Marcos2", "Pinho2", 33));
+        Mockito.when(this.repository.save(pessoaExecucao)).thenReturn(pessoaEsperada.get());
+        Optional pessoaSalva = this.service.update(pessoaExecucao.getId(), pessoaExecucao);
+        Assert.assertEquals(pessoaEsperada, pessoaSalva);
+    }
+
     @Test
-    public void testFindAll() {
+    public void testFindAllSucess() {
         Optional<List<Pessoa>> listaEsperada = Optional.of(Arrays.asList(new Pessoa(), new Pessoa(), new Pessoa()));
         Mockito.when(this.repository.findAll()).thenReturn(Arrays.asList(new Pessoa(), new Pessoa(), new Pessoa()));
         Optional<List<Pessoa>> listaRecebida = this.service.findAll();
@@ -54,7 +72,13 @@ public class PessoaServiceImplTest {
     }
 
     @Test
-    public void testFindById() {
+    public void testFindAllNaoAchouRegistro() {
+        Optional<List<Pessoa>> listaRecebida = this.service.findAll();
+        Assert.assertTrue(listaRecebida.get().isEmpty());
+    }
+
+    @Test
+    public void testFindByIdSucess() {
         Optional<Pessoa> pessoaEsperada = Optional.of(new Pessoa(1L, "Marcos2", "Pinho2", 33));
         Mockito.when(this.repository.findById(1L)).thenReturn(pessoaEsperada);
         Optional pessoaSalva = this.service.findById(1L);
@@ -62,9 +86,20 @@ public class PessoaServiceImplTest {
     }
 
     @Test
-    public void testDelete() {
+    public void testFindByIdNaoAchouRegistro() {
+        Optional pessoaSalva = this.service.findById(1L);
+        Assert.assertFalse(pessoaSalva.isPresent());
+    }
+
+    @Test
+    public void testDeleteSucess() {
         Optional<Pessoa> pessoaEsperada = Optional.of(new Pessoa(1L, "Marcos2", "Pinho2", 33));
         Mockito.when(this.repository.findById(1L)).thenReturn(pessoaEsperada);
+        this.service.delete(1L);
+    }
+
+    @Test(expected = RecurseNotFoundException.class)
+    public void testDeleteComException() {
         this.service.delete(1L);
     }
 }
