028a91d0a61fecf08774070b93f1a1fde1448015
Marcos Pinho <marcosjava2008@gmail.com>
2018-10-28 17:39:55 -0300

    Mehorando o line coverage da API

--- src/main/java/com/marcospinho/demo/dto/PessoaDTO.java
+++ src/main/java/com/marcospinho/demo/dto/PessoaDTO.java
@@ -2,17 +2,13 @@ package com.marcospinho.demo.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.marcospinho.demo.entity.Pessoa;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PessoaDTO implements Serializable {
 
@@ -27,7 +23,10 @@ public class PessoaDTO implements Serializable {
     private Integer idade;
 
     public Pessoa toEntity() {
-        Pessoa pessoa = new Pessoa(this.id, this.nome, this.sobrenome, null);
+        Pessoa pessoa = new Pessoa();
+        pessoa.setId(this.getId());
+        pessoa.setNome(this.getNome());
+        pessoa.setSobrenome(this.getSobrenome());
         pessoa.setIdade(this.getIdade());
         return pessoa;
     }
